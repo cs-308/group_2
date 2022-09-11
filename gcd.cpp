98350485f1156a79a31e6dd828ef120545fc9cde
Harish Choudhary <harishchoudhary2606@gmail.com>
2022-09-11 20:29:36 +0530

    commit to add-rec-fn

--- gcd.cpp
+++ gcd.cpp
@@ -10,8 +10,8 @@ int gcd(int a, int b) {
     return a;
 }
 
-int hcf(int n1, int n2){
-    if(n2!=0)
-        return hcf(n2, n1%n2);
-    else return n1;
+int hcf(int x, int y){
+    if(y!=0)
+        return hcf(y, x%y);
+    else return x;
 }
