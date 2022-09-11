fbc264bbefbd2c19101343c368b3acaf878e8aa8
Harish Choudhary <harishchoudhary2606@gmail.com>
2022-09-11 20:33:18 +0530

    final commit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(){
     int x=56,y=98;
     cout<<gcd(x,y);
+    cout<<hcf(x,y);
     print_hello();
     cout<< endl;
     cout<<"The factorial of 5 is "<< factorial(5)<< endl;
